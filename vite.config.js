9b551e2266ce9e2ff60a9d565a8ac9baa9c23acb
Sam Ayoub <m22386@live.com>
2025-11-08 15:12:15 -0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   // Set this to '/<repo>/' when deploying to GitHub Pages
-  base: '/',
+  base: "/",
   plugins: [react()],
-})
+});
